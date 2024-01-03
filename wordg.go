f3ff6bcabd6599efbb4c1a37f8c19c52989c7ae5
Mark Riordan <riordan@rocketmail.com>
2024-01-02 19:23:50 -0700

    Guess only words that have all required letters
    Also, change cmdline parser and add --word=word cmdline option.

--- wordg.go
+++ wordg.go
@@ -16,18 +16,19 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"strings"
 )
 
-type ParseResult int
+type RunType int
 
 // This is a weird way of creating kind of an enum.
 // The constants are for the different program modes.
 const (
-	BAD ParseResult = iota
+	BAD RunType = iota
 	RUN
 	GUESS
 )
@@ -36,32 +37,53 @@ const LETTERS_IN_WORD = 5
 
 var MyScanner bufio.Scanner
 
+// Map: index is a letter, value is the minimum number of occurrences of that
+// letter in the word we are trying to guess.  We don't populate with letters
+// that we don't yet know are required.
+var requiredLetters = make(map[string]int)
+
+type Settings struct {
+	runType RunType
+	word    string
+	errMsg  string
+}
+
 func usage() {
 	var usageMsg = []string{
 		"wordg: Program to play Wordle.",
-		"Usage: wordg {--run | --guess }",
+		"Usage: wordg {--run | --guess } [--word=word]",
 		"where:",
 		"--run   specifies that the program should think of a word and let you guess it.",
 		"--guess specifies that the program should makes guesses about a word some",
 		"        other entity is thinking of.",
+		"word    applies only to --run mode, and specifies the word the program should",
+		"        think of. Optional; the default is for wordg to select aa word randomly.",
 	}
 	for _, line := range usageMsg {
 		fmt.Println(line)
 	}
 }
 
-func parseCmdLine() ParseResult {
-	result := BAD
-	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) == 1 {
-		arg1 := argsWithoutProg[0]
-		if arg1 == "--run" {
-			result = RUN
-		} else if arg1 == "--guess" {
-			result = GUESS
+func parseCmdLine() Settings {
+	var settings Settings
+	var run bool
+	var guess bool
+	flag.BoolVar(&run, "run", false, "Have the program think of a word and make you guess")
+	flag.BoolVar(&guess, "guess", false, "Have the program try to guess the word")
+	flag.StringVar(&settings.word, "word", "", "The word the program is thinking of in run mode. If not supplied, the program will chose a word at random.")
+
+	flag.Parse()
+
+	if (run && guess) || (!run && !guess) {
+		settings.errMsg = "You must specify either --guess or --run"
+	} else {
+		if run {
+			settings.runType = RUN
+		} else {
+			settings.runType = GUESS
 		}
 	}
-	return result
+	return settings
 }
 
 func isKnownWord(word string) bool {
@@ -81,8 +103,10 @@ func readGuessResult() string {
 	return response
 }
 
-func runGame() {
-	word := AllWords[rand.Intn(len(AllWords))]
+func runGame(word string) {
+	if len(word) == 0 {
+		word = AllWords[rand.Intn(len(AllWords))]
+	}
 	//fmt.Println("The word is " + word)
 	for running := true; running; {
 		fmt.Print(" Guess: ")
@@ -144,9 +168,6 @@ func runGame() {
 			}
 		}
 	}
-	// for _, element := range allWords {
-	// 	fmt.Println(element)
-	// }
 }
 
 // Define a Set type as a map with a boolean value
@@ -172,13 +193,18 @@ func (set StringSet) Contains(element string) bool {
 	return set[element]
 }
 
+// Return true if we found the correct word.
 func processResponse(validLetters *[LETTERS_IN_WORD]StringSet, myGuess string,
 	response string) bool {
 	foundAnswer := false
 	if response == "yyyyy" {
 		foundAnswer = true
+	} else if len(response) != LETTERS_IN_WORD {
+		fmt.Printf("Response must be of length %v\n", LETTERS_IN_WORD)
 	} else {
-		for ipos := 0; ipos < len(validLetters); ipos++ {
+		// Loop through the letters in the response.
+		var charToCountThisGuess map[string]int = make(map[string]int)
+		for ipos := 0; ipos < LETTERS_IN_WORD; ipos++ {
 			respCh := response[ipos : ipos+1]
 			guessCh := myGuess[ipos : ipos+1]
 			if respCh == "n" {
@@ -188,8 +214,37 @@ func processResponse(validLetters *[LETTERS_IN_WORD]StringSet, myGuess string,
 			} else if respCh == "y" {
 				validLetters[ipos].RemoveAll()
 				validLetters[ipos].Add(guessCh)
+
+				_, present := charToCountThisGuess[guessCh]
+				if present {
+					charToCountThisGuess[guessCh]++
+				} else {
+					charToCountThisGuess[guessCh] = 1
+				}
 			} else if respCh == "p" {
 				validLetters[ipos].Remove(guessCh)
+
+				_, present := charToCountThisGuess[guessCh]
+				if present {
+					charToCountThisGuess[guessCh]++
+				} else {
+					charToCountThisGuess[guessCh] = 1
+				}
+			} else {
+				fmt.Println("Unexpected response char: " + respCh)
+			}
+		}
+		// Now we have accumulated in charToCountThisGuess the info from the response
+		// for this guessed word. Apply this knowledge to requiredLetters, which will
+		// reflect required letters info from all responses so far.
+		for requiredCh, count := range charToCountThisGuess {
+			oldCount, present := requiredLetters[requiredCh]
+			if present {
+				if count > oldCount {
+					requiredLetters[requiredCh] = count
+				}
+			} else {
+				requiredLetters[requiredCh] = count
 			}
 		}
 	}
@@ -211,9 +266,26 @@ func printSetOfValidLetters(validLetters *[LETTERS_IN_WORD]StringSet) {
 	}
 }
 
+func makeMapFromWord(word string) map[string]int {
+	mapLetterToCount := make(map[string]int)
+
+	for j := 0; j < len(word); j++ {
+		ch := word[j : j+1]
+		count, present := mapLetterToCount[ch]
+		if present {
+			mapLetterToCount[ch] = count + 1
+		} else {
+			mapLetterToCount[ch] = 1
+		}
+	}
+
+	return mapLetterToCount
+}
+
 func doGuesses() {
 	// Define an array of sets, one for each position in the word being guessed.
 	// Initially populate each set with all possible letters.
+	fmt.Println(("doGuesses here"))
 	var validLetters [LETTERS_IN_WORD]StringSet
 	for i, _ := range validLetters {
 		validLetters[i] = make(map[string]bool)
@@ -233,16 +305,32 @@ func doGuesses() {
 		// that matches the clues we have so far.
 		for _, guess := range AllWords {
 			matches := true
+			// Loop through the letters of this guess.
 			for ilet := 0; ilet < len(guess); ilet++ {
 				if !validLetters[ilet].Contains(guess[ilet : ilet+1]) {
+					// This potential guess is incompatible with the clues so far,
+					// so stop analyzing this potential guess.
 					matches = false
 					break
 				}
 			}
 			if matches {
-				myGuess = guess
-				fmt.Println(myGuess)
-				break
+				// The word matches according to validLetters, but does it have
+				// all the letters we know are in the word?
+				mapLetterToCountThisWord := makeMapFromWord(guess)
+				for letter, numRequired := range requiredLetters {
+					countThisGuess, present := mapLetterToCountThisWord[letter]
+					if !present {
+						matches = false
+					} else if countThisGuess < numRequired {
+						matches = false
+					}
+				}
+				if matches {
+					myGuess = guess
+					fmt.Println(myGuess)
+					break
+				}
 			}
 		}
 		if len(myGuess) == 0 {
@@ -261,13 +349,16 @@ func doGuesses() {
 }
 
 func main() {
-	ParseResult := parseCmdLine()
-	MyScanner = *bufio.NewScanner(os.Stdin)
-	if ParseResult == BAD {
+	settings := parseCmdLine()
+	if len(settings.errMsg) != 0 {
+		fmt.Println(settings.errMsg)
 		usage()
-	} else if ParseResult == GUESS {
-		doGuesses()
-	} else if ParseResult == RUN {
-		runGame()
+	} else {
+		MyScanner = *bufio.NewScanner(os.Stdin)
+		if settings.runType == GUESS {
+			doGuesses()
+		} else if settings.runType == RUN {
+			runGame(settings.word)
+		}
 	}
 }
